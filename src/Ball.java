40a1266ce4d7f528ac72f01c8b7bf3433e661c48
Finn <finnlucajensen555@gmail.com>
2023-04-13 23:13:02 +0200

    Special Ball fixxen!

--- src/Ball.java
+++ src/Ball.java
@@ -4,39 +4,53 @@ import java.util.Random;
 
 public class Ball {
     Random random = new Random();
-    public Fenster window;
-    double xPos;
-    double yPos;
-    double diameter;
-    int color;
-    double speed;
-    double accel = 98.1;
+    private Fenster window;
+    private double xPos;
+    private double yPos;
+    private double diameter;
+    private int color_1;
+    private int color_2;
+    private int color_3;
+    private double speed;
+    private double speed_2 = 10;
+    final double accel = 98.1;
 
-    Ball(Fenster iWindow, double inputPositionX, double inputPositionY, double iDiameter, int iColor, double iSpeed) {
+    Ball(Fenster iWindow, double inputPositionX, double inputPositionY, double iDiameter, int iColor_1, int iColor_2, int iColor_3, double iSpeed) {
         window = iWindow;
         xPos = inputPositionX;
         yPos = inputPositionY;
         diameter = iDiameter;
-        color = iColor;
+        color_1 = iColor_1;
+        color_2 = iColor_2;
+        color_3 = iColor_3;
         speed = iSpeed;
     }
 
     Ball(PApplet window) {
-        xPos = random.nextInt(window.width) + 1;
-        yPos = random.nextInt(window.height) + 1;
-        diameter = random.nextInt(100) + 100;
+        this.window = (Fenster) window;
+        xPos = window.random((float) (0 + diameter / 2), (float) (window.width - diameter / 2));
+        yPos = window.random((float) (0 + diameter / 2), (float) (window.width - diameter / 2));
+        diameter = window.random(100, 200);
+        color_1 = (int) window.random(255);
+        color_2 = (int) window.random(255);
+        color_3 = (int) window.random(255);
+        speed = 0;
     }
 
     void male() {
-        window.circle((float) xPos, (float) yPos, (float) diameter);
+        window.fill(color_1, color_2, color_3);
+        window.ellipse((float) xPos, (float) yPos, (float) diameter, (float) diameter);
     }
 
-    public void move(double t) {
-        speed = speed + accel * t / window.frameRate;
+    void move(double t) {
+        speed = speed + accel * t;
         yPos = yPos + speed * t;
-        if (yPos >= window.height - diameter / 2) {
+        xPos = xPos + speed_2;
+        if (yPos > window.height - diameter / 2) {
             speed = -speed;
-            yPos = yPos + speed * t;
+        }
+        if (xPos > window.width - diameter / 2 || xPos < 0 + diameter / 2) {
+            speed_2 = -speed_2;
         }
     }
 }
--- src/Fenster.java
+++ src/Fenster.java
@@ -4,19 +4,22 @@ import java.util.Random;
 
 public class Fenster extends PApplet {
     Random random = new Random();
-    Ball[] Ball = new Ball[random.nextInt(50)];
+    Ball[] Ball = new Ball[random.nextInt(100)];
 
     @Override
     public void settings() {
         size(800, 800);
         for (int x = 0; x < Ball.length; x++) {
             Ball[x] = new Ball(this);
-            System.out.println("Diameter: " + Ball[x].diameter); //Arrays befüllen klappt super, wie malen?
         }
     }
 
     @Override
     public void draw() {
         background(200, 100, 100);
+        for (int x = 0; x < Ball.length; x++) {
+            Ball[x].male();
+            Ball[x].move(1 / frameRate);
+        }
     }
 }
